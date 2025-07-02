7427198efc38f0aaea22440cac7848b2bf35d16a
Ram Saran <183719077+divyaramsaran@users.noreply.github.com>
2025-07-02 16:26:42 +0000

    adds configurations and apis list

---
+++ dataAggregator.js
@@ -0,0 +1,11 @@
+const configuration = {
+  retryCount: 3,
+  retryDelay: 1000,
+  retry: true,
+};
+
+const apis = [
+  "https://restcountries.com/v3.1/all",
+  "https://api.example.com/data",
+  "https://api.example.com/more-data",
+];
